e88ce80febebe1c3f69fd90a41f414d38520e61a
Carrie Kralovec <ckralove@ch275l-05.adit.mines.edu>
2017-04-05 16:20:58 -0600

    Updated main to loop through time

--- initialserial.f90
+++ initialserial.f90
@@ -10,18 +10,35 @@ program initialserial
 
   ! Create temporary grid same size as master
   grid_row = size(master_grid,1)
-  grid_col = size(master_grid,2
+  grid_col = size(master_grid,2)
   allocate(new_grid(grid_row,grid_col))
 
-  ! Do one step of the numerical method 
-  CALL doStep(master_grid, new_grid, grid_row, grid_col)
-
-  
-  ! Write the grid to a file 
+  ! Open up file
   open(unit = 20, file = "test.txt")
-  do i = 1,grid_row
-     write(20, *) (new_grid(i,j), j = 1,grid_col)
+
+  do i = 1, grid_row
+     write(20, *) (master_grid(i,j), j = 1, grid_col)
   end do
+  write(20, *) ''
+  
+  do n = 1,10
+  
+     ! Do one step of the numerical method
+     CALL doStep(master_grid, new_grid, grid_row, grid_col)
+
+
+     ! Write the next time step grid to a file 
+     do i = 1, grid_row
+        write(20, *) (new_grid(i,j), j = 1, grid_col)
+     end do
+     
+     write(20, *) ''
+     
+     master_grid = new_grid 
+
+  end do
+
+  ! Close file
   close(20)
 
      
