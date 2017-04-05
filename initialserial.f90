a20c5faf7ac9dd2ee5ff933890bd09ecd695f37b
Carrie Kralovec <ckralove@ch275l-05.adit.mines.edu>
2017-04-05 15:25:45 -0600

    Updated main to do one update step

--- initialserial.f90
+++ initialserial.f90
@@ -2,22 +2,29 @@ program initialserial
   use initialMod
   implicit none
 
-  integer :: i, j, n
+  integer :: i, j, n, grid_row, grid_col
   double precision, allocatable, dimension(:,:) :: master_grid, new_grid
 
-
+  ! Initialize the grid 
   CALL initializeGrid(master_grid)
-  
-  open(unit = 20, file = "test.txt")
-  
-  do i = 1,size(master_grid,1)
-     write(20, *) (master_grid(i,j), j = 1,size(master_grid,2))
-  end do
 
+  ! Create temporary grid same size as master
+  grid_row = size(master_grid,1)
+  grid_col = size(master_grid,2
+  allocate(new_grid(grid_row,grid_col))
+
+  ! Do one step of the numerical method 
+  CALL doStep(master_grid, new_grid, grid_row, grid_col)
+
+  
+  ! Write the grid to a file 
+  open(unit = 20, file = "test.txt")
+  do i = 1,grid_row
+     write(20, *) (new_grid(i,j), j = 1,grid_col)
+  end do
   close(20)
 
      
-  
 
 end program initialserial
 
