cef75e610c6e59b4cb5dc5b60207854e8244204e
Justin Hood <juhood@ch275l-02.adit.mines.edu>
2017-04-19 18:12:34 -0600

    Added matlab file

---
+++ PlotScript.m
@@ -0,0 +1,31 @@
+sizer=5;
+depth=10;
+
+fileId=fopen('test.txt', 'r');
+matSize=[sizer,sizer];
+
+formatSpec='';
+for i=1:sizer
+    formatSpec=strcat(formatSpec, ' %f');
+end
+formatSpec=formatSpec(2:end)
+
+x=zeros(sizer, sizer, depth);
+
+for i=1:depth
+    temp=fscanf(fileId, formatSpec, matSize);
+    x(:,:,i)=temp;
+end
+x(:,:,1);
+x(:,:,2);
+
+figure(1)
+
+a=linspace(0,4,sizer);
+b=linspace(0,4,sizer);
+
+for t=1:depth
+    s=surf(a,b,x(:,:,t))
+    %zlim([0 10])
+    pause(0.05)
+end
